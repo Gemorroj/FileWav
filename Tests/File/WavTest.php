775e57a9f3e97ac4ffd609ed3015b75994be0969
Gemorroj <wapinet@gmail.com>
2015-12-12 15:30:14 +0300

    add package.xml, add php 7.0 to travis

---
+++ Tests/File/WavTest.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'File/Wav.php';
+
+class File_WavTest extends PHPUnit_Framework_TestCase
+{
+    public function testWav()
+    {
+        $this->markTestSkipped('Not implemented yet');
+    }
+}
